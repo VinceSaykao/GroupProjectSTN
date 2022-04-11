f015ace9235695df6be9cc01ad4413caa0199d5a
Tony LaForgia <alforgia428@gmail.com>
2022-04-11 15:25:15 -0500

    created fetchProfileEvent saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,6 +3,7 @@ import loginSaga from "./login.saga";
 import registrationSaga from "./registration.saga";
 import userSaga from "./user.saga";
 import fetchProfile from "./Profile_SAGA/fetchProfile.saga";
+import fetchProfileEvent from "../reducers/Profile_Reducer/fetchProfileEvent.reducer";
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -16,5 +17,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     fetchProfile(),
+    fetchProfileEvent(),
   ]);
 }
