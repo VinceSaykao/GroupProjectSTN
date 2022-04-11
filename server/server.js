71bd82a1471059e61eaf17f4105b91f6f8ce65f7
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-11 09:53:25 -0500

    made backend files and app use in server file imports

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const profileRouter = require('./routes/profile.router');
+const organizationRouter = require('./routes/organization.router');
+const eventRouter = require('./routes/event.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +26,9 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/organization', organizationRouter);
+app.use('/api/event', eventRouter);
 
 // Serve static files
 app.use(express.static('build'));
