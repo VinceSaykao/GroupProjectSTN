881434425319aa04f7e6afc241be29c11e79dbf2
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-12 21:50:21 -0500

    inserted pending event reducer

---
+++ src/redux/reducers/Event_Reducers/fetchPendingEvents.reducer.js
@@ -0,0 +1,11 @@
+const fetchPendingEvents = (state = [], action) => {
+    switch (action.type) {
+      case "SET_FETCH_PENDING_EVENTS":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export default fetchPendingEvents;
+  
