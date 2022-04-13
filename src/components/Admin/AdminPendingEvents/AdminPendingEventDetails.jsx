5b89c49febbc7a3701136e7eb8d65ecbf05bd966
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-13 08:31:45 -0500

    added mui buttons

--- src/components/Admin/AdminPendingEvents/AdminPendingEventDetails.jsx
+++ src/components/Admin/AdminPendingEvents/AdminPendingEventDetails.jsx
@@ -3,6 +3,12 @@ import { useSelector } from "react-redux";
 
 import "./AdminPendingEventDetails.scss";
 
+//MUI
+import Button from '@mui/material/Button';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CloseIcon from '@mui/icons-material/Close';
+
 export default function AdminPendingEventDetails() {
 
     const fetchEventId = useSelector(store => store.fetchEventId)
@@ -21,6 +27,13 @@ export default function AdminPendingEventDetails() {
                 return (
                     <div id={i}>
                     <p>You Live In: </p>{detail.city}
+                    <Button variant="contained" startIcon={<CheckCircleOutlineIcon fontSize='large'/>}>
+                Approve
+            </Button>
+
+            <Button variant="contained" startIcon={<CloseIcon fontSize='large'/>}>
+                Deny
+            </Button>
                     </div>
                 )
             })}
