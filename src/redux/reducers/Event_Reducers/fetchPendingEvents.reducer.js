fb010358bfcfa81ce8a2f4d3d837ba487a0b03ad
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-12 21:51:08 -0500

    added pending event in root reducer

--- src/redux/reducers/Event_Reducers/fetchPendingEvents.reducer.js
+++ src/redux/reducers/Event_Reducers/fetchPendingEvents.reducer.js
@@ -1,11 +1,10 @@
 const fetchPendingEvents = (state = [], action) => {
     switch (action.type) {
-      case "SET_FETCH_PENDING_EVENTS":
-        return action.payload;
-      default:
-        return state;
+        case "SET_FETCH_PENDING_EVENTS":
+            return action.payload;
+        default:
+            return state;
     }
-  };
-  
-  export default fetchPendingEvents;
-  
+};
+
+export default fetchPendingEvents;
