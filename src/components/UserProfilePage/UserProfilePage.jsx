202a58d4f2591200c111a1f22b6a5aa1449d15ca
Tony LaForgia <alforgia428@gmail.com>
2022-04-14 18:07:12 -0500

    added to useEffect

--- src/components/UserProfilePage/UserProfilePage.jsx
+++ src/components/UserProfilePage/UserProfilePage.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import UserProfileItem from "./UserProfileItem";
 import UserSavedProfileEvent from "./UserSavedProfileEvent";
+import { actionChannel } from "redux-saga/effects";
 
 export default function UserProfile() {
   const dispatch = useDispatch();
@@ -10,9 +11,35 @@ export default function UserProfile() {
   const user = useSelector((store) => store.user);
 
   useEffect(() => {
-    dispatch({ type: "SET_PROFILE_SAGA", payload: user.id });
-    dispatch({ type: "SET_PROFILE_EVENT_SAGA", payload: user.id });
+    console.log("fetchProfile = ", fetchProfile[0]);
+    console.log(fetchProfile !== []);
+    if (user?.id !== null && user?.id !== undefined) {
+      console.log("user.id = " + user?.id);
+      console.log("user.id is of type " + typeof user?.id);
+    }
+    if (fetchProfile === []) {
+      console.log("fetchProfile contains an empty array");
+    }
+    if (fetchProfile[0] === undefined) {
+      console.log("fetchProfile[0] is undefined.");
+    } else {
+      if (fetchProfile[0]?.id === undefined) {
+        console.log("id is undefined.");
+      }
+      if (fetchProfile[0]?.id === null) {
+        console.log("id is null.");
+      }
+      if (fetchProfile[0]?.id !== null && fetchProfile[0]?.id !== undefined) {
+        console.log("id is of type." + typeof fetchProfile[0]?.id);
+      }
+    }
+    if (fetchProfile !== [] && user.id) {
+      dispatch({ type: "SET_PROFILE_SAGA", payload: user.id });
+      dispatch({ type: "SET_PROFILE_EVENT_SAGA", payload: user.id });
+    }
   }, []);
+  setTimeout(() => {
+  }, 300);
 
   console.log("profile", fetchProfile);
   console.log("user", user.id);
@@ -22,7 +49,7 @@ export default function UserProfile() {
       <div>
         <h3>Profile</h3>
 
-        {fetchProfile.id}
+        {fetchProfile[0]?.id}
         <div>
           <p>{fetchProfile[0]?.first_name}</p>
           <p>{fetchProfile[0]?.last_name}</p>
