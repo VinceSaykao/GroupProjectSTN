4adc807cb70bed3cd9eb401efefc16815202e7df
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-14 19:39:57 -0500

    restyle grid for admin list

--- src/components/Admin/AdminApprovedEvents/AdminEventListItem.jsx
+++ src/components/Admin/AdminApprovedEvents/AdminEventListItem.jsx
@@ -1,5 +1,6 @@
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
+import * as React from 'react';
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -25,6 +26,9 @@ const Img = styled('img')({
 });
 
 
+
+
+
 export default function AdminEventListItem({ event }) {
 
 
@@ -41,13 +45,62 @@ export default function AdminEventListItem({ event }) {
         history.push('/admin-approved-event-details')
     }; // end of handleClick
 
+
+    const StyledPaper = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        maxWidth: 400,
+        color: theme.palette.text.primary,
+    }));
+
+    const message = `Truncation should be conditionally applicable on this long line of text
+      as this is a much longer line than what the container can support. `;
+
+
+
+
+
     return (
 
         <div
             className='admin-event-item'
             onClick={handleClick}>
 
-            <Paper
+            <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
+                <StyledPaper
+                    sx={{
+                        my: 1,
+                        mx: 'auto',
+                        p: 2,
+                    }}
+                >
+                    <Grid container wrap="nowrap" spacing={2}>
+                        <Grid item>
+                            <ButtonBase sx={{ width: 70, height: 1 }}>
+                                <Avatar
+                                    sx={{ width: 80, height: 80 }}
+                                    src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" />
+                            </ButtonBase>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography noWrap>{event.date}</Typography>
+                            <Typography noWrap>{event.name} <span className='i'>|</span></Typography>
+                            <Typography noWrap>{event.city}</Typography>
+
+                        </Grid>
+
+                            <ArrowForwardIosIcon
+                            className='admin-arrow'
+                                fontSize='large'
+                            />
+                
+                    </Grid>
+                </StyledPaper>
+
+            </Box>
+
+            {/* <Paper
                 sx={{
                     p: 1,
                     margin: 1,
@@ -59,35 +112,38 @@ export default function AdminEventListItem({ event }) {
             >
                 <Grid container spacing={1}>
                     <Grid item>
-                        <ButtonBase sx={{ width: 60, height: 1 }}>
-                            <Avatar 
-                            sx={{ width: 56, height: 56 }}
-                            alt="Remy Sharp" src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" />
+                        <ButtonBase sx={{ width: 70, height: 1 }}>
+                            <Avatar
+                                sx={{ width: 80, height: 80 }}
+                                alt="Remy Sharp" src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" />
                         </ButtonBase>
                     </Grid>
                     <Grid item xs={9.5} sm container>
                         <Grid item xs container direction="column" spacing={5}>
-                            <Grid item xs>
+                            <Grid item xs >
                                 <Typography gutterBottom variant="subtitle1" component="div">
                                     {event.city}
+                                    <br></br>
                                     {event.date}
+                                    <br></br>
+                                    {event.name}
                                 </Typography>
-                            
+
                             </Grid>
-                        
+
                         </Grid>
                         <Grid >
-                            <Typography 
-                            align='justify'
-                            variant="subtitle1" component="div">
-                            <ArrowForwardIosIcon 
-                            fontSize='large'
-                            />
-                            </Typography>
+                            <ButtonBase sx={{ width: 50, height: 1 }}
+                                align='justify'
+                                variant="subtitle1" component="div">
+                                <ArrowForwardIosIcon
+                                    fontSize='large'
+                                />
+                            </ButtonBase>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Paper>
+            </Paper> */}
 
 
 
