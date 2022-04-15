f0b0fb76fb151b05c81bb6829d808ea67070b705
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-14 21:39:55 -0500

    added new items in detail page

--- src/components/Admin/AdminApprovedEvents/AdminEventDetails.jsx
+++ src/components/Admin/AdminApprovedEvents/AdminEventDetails.jsx
@@ -4,6 +4,10 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 
@@ -28,6 +32,7 @@ export default function AdminEventDetails() {
 
 
 
+    console.log('detail', fetchEventId) // need to fetch month and date
     return (
         <div>
             <Helmet>
@@ -36,46 +41,62 @@ export default function AdminEventDetails() {
                 </style>
             </Helmet>
 
-            {/* <div className="event-approved-list-container">
-            {fetchEventId.map((detail, i) => {
-                return (
-                    <div id={i}>
-                        <p>You Live In: </p>{detail.city}
-                    </div>
-                )
-            })}
-            </div> */}
+            <div className="event-approved-list-container">
+                {fetchEventId.map((detail, i) => {
+                    return (
+                        <div id={i}>
+
+                            <Box sx={{ flexGrow: 1 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <Paper
+                                            sx={{
+                                                height: 140,
+                                            }}
+                                        >
+                                            <img
+                                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                                loading="lazy"
+                                                height="140px"
+                                                width="100%"
+                                            />
+                                        </Paper>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Item>{detail.name}</Item>
+                                        <Grid item xs={12}>
+
+                                            <Item>
+                                                <Button
+                                                    variant="contained"
+                                                    startIcon={<EditIcon />}
+                                                >Edit</Button>
+                                                <Button
+                                                    startIcon={<ContentCopyIcon />}
+                                                    variant="contained"
+                                                >Copy</Button>
+                                                <Button variant="contained" startIcon={<DeleteIcon />}>
+                                                    Delete
+                                                </Button>
+                                            </Item>
+                                        </Grid>
+                                    </Grid>
 
 
 
 
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Paper
-                            sx={{
-                                height: 140,
-                            }}
-                        >
-                            <img
-                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                                loading="lazy"
-                                height="140px"
-                                width="100%"
-                            />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Item>xs=8</Item>
-                    </Grid>
-                </Grid>
-            </Box>
+                                    <Grid item xs={12}>
+                                        <Item>{detail.description}</Item>
+                                        <Item>{detail.month}</Item>
+                                        <Item>{detail.description}</Item>
+                                    </Grid>
+
+                                </Grid>
+                            </Box>
+                        </div>
+                    )
+                })}
+            </div>
 
         </div>
     )
