7fc366eb0460d55a1c5fc6bacd6e3a6343668acc
Andrew Kapp <dkapp59@gmail.com>
2022-04-18 16:31:40 -0500

    Updated organization view

--- src/components/Organization/OrganizationEventsList.jsx
+++ src/components/Organization/OrganizationEventsList.jsx
@@ -1,48 +1,38 @@
-import AdminEventListItem from "../Admin/AdminApprovedEvents/AdminEventListItem";
+import OrgEventListItem from './OrgEventListItem';
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 
-// import './AdminEventList.scss';
-
+import './OrganizationView.css';
 
 export default function OrganizationEventsList() {
+  // store that grabs approved events, although fetchApprovedEvents brings in more data...??
+  const approvedEvents = useSelector((store) => store.fetchApprovedEvents);
+  const user = useSelector((store) => store.user);
 
-    // store that grabs approved events, although fetchApprovedEvents brings in more data...??
-    const approvedEvents = useSelector(store => store.fetchApprovedEvents);
-    const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  // useEffect to grab the approved events
+  useEffect(() => {
+    dispatch({ type: 'FETCH_APPROVED_EVENTS' });
+  }, []);
 
-    // useEffect to grab the approved events
-    useEffect(() => {
-        dispatch({ type: 'FETCH_APPROVED_EVENTS' });
-    }, []);
+  const orgEvents = approvedEvents.filter(
+    (approvedEvents) => approvedEvents.org_id === user.org_id
+  );
 
-    const orgEvents = approvedEvents.filter(approvedEvents => approvedEvents.org_id === user.org_id)
-
-    return (
-        <div className="admin-event-view">
-
-            <div className="approved-event-list">
-                {orgEvents?.map((event, i) => {
-                    return (
-                        <div key={i}>
-
-                     
-                                <AdminEventListItem
-                                event={event}
-                            />
-                        
-                        </div>
-                    )
-                })}
+  return (
+    <div className="org-event-view">
+      <div className="org-event-list">
+        {orgEvents?.map((event, i) => {
+          return (
+            <div key={i}>
+              <OrgEventListItem event={event} />
             </div>
-
-
-
-        </div>
-    )
-
-}; 
+          );
+        })}
+      </div>
+    </div>
+  );
+}
