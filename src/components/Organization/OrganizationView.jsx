63b4e82763aa04ba11e36d0e151a0d41a03a2371
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-20 18:57:59 -0500

    updated margin on nav bar and made event title

--- src/components/Organization/OrganizationView.jsx
+++ src/components/Organization/OrganizationView.jsx
@@ -44,100 +44,7 @@ function OrganizationView() {
 
   return (
     <div>
-    <Box className="org-view">
-      <Grid container justifyContent="center">
-        <div className="org-box">
-          <Box
-            component="img"
-            sx={{
-              height: 'auto',
-              width: '400px',
-              maxHeight: { xs: 233, md: 167 },
-              maxWidth: { xs: 350, md: 250 },
-            }}
-            alt=""
-            src={org.image}
-          />
-        </div>
-        <Typography gutterBottom variant="h5" component="div" color="white">
-          {org.name}
-        </Typography>
-      </Grid>
-      {user.access_level === 1 ? (
-        <div></div>
-      ) : (
-        <div>
-          {
-            <Box
-              sx={{
-                height: 25,
-                display: 'flex',
-                pr: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                mb: 1
-              }}
-            >
-              <Button
-                className="edit-button"
-                size="small"
-                variant="contained"
-                onClick={handleEditClick}
-              >
-                <EditIcon fontSize="small" />
-                Edit
-              </Button>
-            </Box>
-          }
-        </div>
-      )}
-
-      <Typography gutterBottom variant="body1" component="div" color="white" sx={{ m:2 }}>
-        {org.description}
-      </Typography>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{
-            backgroundColor: "rgb(101, 101, 101)",
-            color: "white"
-        }}
-        >
-          <Typography>Contact Info & Location</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography gutterBottom variant="body1" component="div">
-            {org.website}
-          </Typography>
-          <Typography gutterBottom variant="body1" component="div">
-            {org.email}
-          </Typography>
-          <Typography gutterBottom variant="body1" component="div">
-            {org.phone}
-          </Typography>
-          <Typography gutterBottom variant="body1" component="div">
-            {org.address1} {org.address2} {org.city}, {org.state} {org.zip}
-          </Typography>
-          <Grid container justifyContent="center">
-            <iframe
-              width="300"
-              height="200"
-              frameBorder={0}
-              style={{ border: 0 }}
-              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDX4e7v69d8lQVeWvBOcs-Bt9mFS2VVogg&q=${org.address1}${org.address2}${org.city}${org.state}${org.zip}`}
-              allowFullScreen
-            ></iframe>
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
-      <OrganizationEventsList />
-    </Box>
-
-
-
-    {/* <BottomNavigation
+      <BottomNavigation
         sx={{ width: 'auto' }}
         style={{ backgroundColor: 'rgb(75, 75, 75)' }}
       >
@@ -180,7 +87,106 @@ function OrganizationView() {
             }
           </div>
         )}
-      </BottomNavigation> */}
+      </BottomNavigation>
+      <Box className="org-view">
+        <Grid container justifyContent="center">
+          <div className="org-box">
+            <Box
+              component="img"
+              sx={{
+                height: 'auto',
+                width: '400px',
+                maxHeight: { xs: 233, md: 167 },
+                maxWidth: { xs: 350, md: 250 },
+              }}
+              alt=""
+              src={org.image}
+            />
+          </div>
+          <Typography gutterBottom variant="h5" component="div" color="white">
+            {org.name}
+          </Typography>
+        </Grid>
+        {user.access_level === 1 ? (
+          <div></div>
+        ) : (
+          <div>
+            {
+              <Box
+                sx={{
+                  height: 25,
+                  display: 'flex',
+                  pr: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  mb: 1
+                }}
+              >
+                <Button
+                  className="edit-button"
+                  size="small"
+                  variant="contained"
+                  onClick={handleEditClick}
+                >
+                  <EditIcon fontSize="small" />
+                  Edit
+                </Button>
+              </Box>
+            }
+          </div>
+        )}
+
+        <Typography gutterBottom variant="body1" component="div" color="white" sx={{ m: 2 }}>
+          {org.description}
+        </Typography>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{
+              backgroundColor: "rgb(101, 101, 101)",
+              color: "white"
+            }}
+          >
+            <Typography>Contact Info & Location</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography gutterBottom variant="body1" component="div">
+              {org.website}
+            </Typography>
+            <Typography gutterBottom variant="body1" component="div">
+              {org.email}
+            </Typography>
+            <Typography gutterBottom variant="body1" component="div">
+              {org.phone}
+            </Typography>
+            <Typography gutterBottom variant="body1" component="div">
+              {org.address1} {org.address2} {org.city}, {org.state} {org.zip}
+            </Typography>
+            <Grid container justifyContent="center">
+              <iframe
+                width="300"
+                height="200"
+                frameBorder={0}
+                style={{ border: 0 }}
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDX4e7v69d8lQVeWvBOcs-Bt9mFS2VVogg&q=${org.address1}${org.address2}${org.city}${org.state}${org.zip}`}
+                allowFullScreen
+              ></iframe>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+        <h1
+        className='organization-event-header'
+        >Events</h1>
+        <OrganizationEventsList />
+
+
+      </Box>
+
+
+
+
     </div>
   );
 }
