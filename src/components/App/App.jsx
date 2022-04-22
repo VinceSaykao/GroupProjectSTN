cb4470923298b6c46d6a234c9d73d4af584e80ea
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-04-21 19:02:45 -0500

    uncomment app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,7 +21,6 @@ import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
-
 // User Profile
 import UserProfilePage from '../UserProfilePage/UserProfilePage';
 import UserProfileEditForm from '../UserProfilePage/UserProfileEditForm';
@@ -54,54 +53,54 @@ function App() {
   // fetch approved events
   const events = useSelector(store => store.fetchAdminEvents);
 
-  console.log('this is events: ', events)
+  
+  const expiredEvents = events.filter(
+    (events) => events.stop_date < dateNow
+    );
+    
+    console.log('this is events: ', events)
+      console.log('this filters: ', expiredEvents );
 
-//     const expiredEvents = events.filter(
-//     (events) => events.stop_date < dateNow
-//   );
+const something = () => {
 
-//   console.log('this filters: ', expiredEvents );
+  console.log('dsajfsdjflkdjfkdfk')
+  {expiredEvents.map((exp) => {
 
-// const something = () => {
-
-//   console.log('dsajfsdjflkdjfkdfk')
-//   {expiredEvents.map((exp) => {
-
-//     const dateNow = format(new Date(), 'yyyy/MM/dd')
-//     if (exp.end_date < dateNow) {
-//       console.log('expired!!!!!!!!!!!!!!!!')
-//       dispatch({
-//           type: 'UPDATE_EVENT',
-//           payload:
-//           {
-//               id: exp.id,
-//               org_id: exp.org_id,
-//               category_id: exp.category_id,
-//               status: 'expired',
-//               name: exp.name,
-//               description: exp.description,
-//               date: exp.date,
-//               link: exp.link,
-//               start_date: exp.start_date,
-//               end_date: exp.end_date,
-//               start_time: exp.start_time,
-//               end_time: exp.end_time,
-//               image: exp.image,
-//               email: exp.email,
-//               phone: exp.phone,
-//               address1: exp.address1,
-//               address2: exp.address2,
-//               city: exp.city,
-//               zip: exp.zip,
-//               state: exp.state,
-//               feedback: exp.feedback,
-//           }
-//       });
-//   } else {
-//       console.log('not expired');
-//   }
-//   })}
-// }
+    const dateNow = format(new Date(), 'yyyy/MM/dd')
+    if (exp.end_date < dateNow) {
+      console.log('expired!!!!!!!!!!!!!!!!')
+      dispatch({
+          type: 'UPDATE_EVENT',
+          payload:
+          {
+              id: exp.id,
+              org_id: exp.org_id,
+              category_id: exp.category_id,
+              status: 'expired',
+              name: exp.name,
+              description: exp.description,
+              date: exp.date,
+              link: exp.link,
+              start_date: exp.start_date,
+              end_date: exp.end_date,
+              start_time: exp.start_time,
+              end_time: exp.end_time,
+              image: exp.image,
+              email: exp.email,
+              phone: exp.phone,
+              address1: exp.address1,
+              address2: exp.address2,
+              city: exp.city,
+              zip: exp.zip,
+              state: exp.state,
+              feedback: exp.feedback,
+          }
+      });
+  } else {
+      console.log('not expired');
+  }
+  })}
+}
 
 
   // dispatch saga -> loop for each event.id axios.put /:id ,  run the put
